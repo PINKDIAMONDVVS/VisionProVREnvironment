0108925387c8b87b93ae46b3b62d015cb7487ba7
PINKDIAMONDVVS <chenjunyi531@gmail.com>
2024-02-14 16:42:49 -0500

    commit 12

--- VisionProVREnvironment/VisionProVREnvironmentApp.swift
+++ VisionProVREnvironment/VisionProVREnvironmentApp.swift
@@ -9,9 +9,22 @@ import SwiftUI
 
 @main
 struct VisionProVREnvironmentApp: App {
+    
+    @State var immersionMode: ImmersionStyle = .full
+    
     var body: some Scene {
+        
+        // Starting Window
         WindowGroup {
             ContentView()
         }
+        .defaultSize(width: 10, height: 10)
+        .windowStyle(.plain)
+        
+        // VR
+        ImmersiveSpace (id: "ImmersiveView") {
+            // VR View
+        }
+        .immersionStyle(selection: $immersionMode, in: .full)
     }
 }

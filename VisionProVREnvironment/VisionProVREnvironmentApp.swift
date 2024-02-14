c0a469e216c99ed0676976090aae1e926b8144f6
PINKDIAMONDVVS <chenjunyi531@gmail.com>
2024-02-14 15:57:37 -0500

    Initial Commit

---
+++ VisionProVREnvironment/VisionProVREnvironmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionProVREnvironmentApp.swift
+//  VisionProVREnvironment
+//
+//  Created by Junyi Chen on 2/14/24.
+//
+
+import SwiftUI
+
+@main
+struct VisionProVREnvironmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
